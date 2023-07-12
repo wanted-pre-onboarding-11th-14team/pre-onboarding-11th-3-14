ed8c020afd8235ebbac9eb94e29f77142be6429f
장윤신 <99275134+yun-sin@users.noreply.github.com>
2023-07-12 18:47:12 +0900

    Design: 로딩,에러 css 변경

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,31 @@
 import React, { memo } from 'react';
+import styled from 'styled-components';
 
 const Loading = memo(() => {
-  return <div>로딩중!..</div>;
+  return (
+    <Background>
+      <LoadingText>Loading . . .</LoadingText>
+    </Background>
+  );
 });
+
+const Background = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: #ffffffb7;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoadingText = styled.div`
+  font: 2rem 'Noto Sans KR';
+  text-align: center;
+`;
 
 export default Loading;
--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,35 @@
 import React, { memo } from 'react';
+import styled from 'styled-components';
 
 const Error = memo(() => {
-  return <div>Error</div>;
+  return (
+    <Background>
+      <ErrorText>
+        😢
+        <br />
+        Not Found
+      </ErrorText>
+    </Background>
+  );
 });
+
+const Background = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: #ffffffb7;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ErrorText = styled.div`
+  font: 2rem 'Noto Sans KR';
+  text-align: center;
+`;
 
 export default Error;
