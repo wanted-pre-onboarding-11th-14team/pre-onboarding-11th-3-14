108205019013a171e1d684e316d10d53b9e3cd17
장윤신 <99275134+yun-sin@users.noreply.github.com>
2023-07-12 14:36:07 +0900

    Feat: Header Name 표시 위한 ContextAPI 연동

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,28 @@
 import React, { memo } from 'react';
+import styled from 'styled-components';
+import { useIssueState } from '../api/IssueContext';
 
 const Header = memo(() => {
-  return <div>Header</div>;
+  const state = useIssueState();
+  const data = (state.issueList.data && state.issueList.data[0]) || state.issue.data;
+
+  return (
+    <div>
+      <HeaderContainer>
+        {data && data.repository_url.split('/').reverse()[1] + ' / '}
+        {data && data.repository_url.split('/').reverse()[0]}
+      </HeaderContainer>
+    </div>
+  );
 });
+
+const HeaderContainer = styled.div`
+  height: 50px;
+  font-size: 30px;
+  margin-bottom: 20px;
+  background-color: gray;
+  line-height: 50px;
+  text-align: center;
+`;
 
 export default Header;
