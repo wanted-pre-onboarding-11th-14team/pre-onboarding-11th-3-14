69b4d33d801eba83a4893a41c18b12d7f529b2a2
장윤신 <99275134+yun-sin@users.noreply.github.com>
2023-07-12 14:32:30 +0900

    Feat: index.js에 Context API Provider 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { IssueProvider } from './api/IssueContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <BrowserRouter>
-    <App />
+    <IssueProvider>
+      <App />
+    </IssueProvider>
   </BrowserRouter>,
 );
