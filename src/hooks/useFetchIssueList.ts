85171e36c6ffa179fadbf39ee474bcc6ab02a20e
khakaa <phr0210@naver.com>
2023-07-13 13:30:22 +0900

    [Refactor] : Issue Type any -> index signature

--- src/hooks/useFetchIssueList.ts
+++ src/hooks/useFetchIssueList.ts
@@ -2,10 +2,12 @@ import { useEffect, useState, useCallback } from 'react';
 import { useIssueState, useIssueDispatch, getIssueList } from '../api/IssueContext';
 import { useIntersectionObserver } from './useIntersectionObserver';
 
+type Issue = { [key: string]: any };
+
 function useFetchIssueList() {
   const state = useIssueState();
   const dispatch = useIssueDispatch();
-  const [data, setData] = useState<any>([]);
+  const [data, setData] = useState<Issue>([]);
   const [page, setPage] = useState(1);
   const { data: issueList, loading, error } = state.issueList;
 
@@ -22,7 +24,7 @@ function useFetchIssueList() {
 
   useEffect(() => {
     if (issueList) {
-      setData((data: any[]) => [...data, ...issueList]);
+      setData((data: Issue[]) => [...data, ...issueList]);
     }
   }, [issueList]);
 
